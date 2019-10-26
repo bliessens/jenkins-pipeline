6f0d619ef1a1c4e2bd5f8bf9aa49e7e8080421b2
bliessens <benoni29@gmail.com>
2019-10-26 21:36:57 +0200

    junit test

---
+++ src/test/java/be/benoit/SampleTest.java
@@ -0,0 +1,13 @@
+package be.benoit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    public void name() {
+        assertTrue(true);
+    }
+}
